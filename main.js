ccccc7aa22f579387e3c654fda675fe6ad435edc
Ishita Sinha <34679447+Ishitasinha7089@users.noreply.github.com>
2021-07-30 18:35:17 +0530

    added files

--- main.js
+++ main.js
@@ -1,32 +1,127 @@
+const textInputs = document.querySelectorAll('.empInputs1518 input[type="text"]')
+const textlabels = document.querySelectorAll('.empInputs1518 label')
+textInputs.forEach(element => {
+    element.addEventListener('focus', () =>{
+        element.nextElementSibling.classList.add('empLabelFocused1518')
+    })
 
-changeTheme = (e) =>{
-    var logo = document.querySelector('.header .logo')
-    if(e.src.includes('sun')){
-        e.src = 'assets/moon.svg'
-        logo.src = 'assets/groww-light.png'
-        localStorage.setItem('theme','dark')
-        document.body.classList.add('dark')
+    element.addEventListener('blur', (event) =>{
+        if(event.target.value===''){
+            element.nextElementSibling.classList.remove('empLabelFocused1518')
+            return;
+        }
+    })
+     element.addEventListener('keyup', (event) =>showError(event.srcElement));
+});
+
+showError = (event) =>{
+    const value = event.value;
+    const inputLabel = event.nextElementSibling.innerHTML
+    if(value==='' || value.trim().length===0){
+        console.log(value, inputLabel, event.parentElement.nextElementSibling.innerHTML);
+        event.parentElement.nextElementSibling.innerHTML= "Please provide your "+ inputLabel.toLowerCase()
+
+    }
+     else{
+        event.parentElement.nextElementSibling.innerHTML=""
+     }
+}
+
+const textArea = document.getElementById('empOtherDetails1518')
+const tandcCheckbox = document.getElementById('empTandC1518');
+textArea.addEventListener('focus',() =>{
+    textArea.nextElementSibling.classList.add('empLabelFocused1518')
+})
+textArea.addEventListener('blur',(event) =>{
+    if(event.target.value===''){
+        textArea.nextElementSibling.classList.remove('empLabelFocused1518')
         return;
     }
-    e.src='assets/sun.svg'
+})
+
+
+
+
+
+changeTheme = (e) =>{
+    var logo = document.querySelector('.empHeader1518 .logo1518')
+    if(e.srcElement.src.includes('sun')){
+        e.srcElement.src = 'assets/moon.svg'
+        logo.src = 'assets/groww-light.png'
+        document.documentElement.setAttribute('data-theme', 'dark')
+        return;
+    }
+    e.srcElement.src='assets/sun.svg'
     logo.src = 'assets/groww-dark.png'
-    localStorage.setItem('theme','light')
-    document.body.classList.remove('dark')
+    document.documentElement.removeAttribute('data-theme')
+}
+toggleModal = () =>{
+    document.getElementsByClassName('empThankyouModal1518')[0].classList.toggle('empThankyouModalOpen1518')
 }
 
-enableSpouseNameField = () =>{
-    document.getElementById('spouseName').disabled = false
+
+toggleSpouseNameField = () =>{
+    const element = document.getElementsByClassName('empSpouseName1518')[0];
+    element.classList.toggle('disableSpouseInput');
+    if(element.classList.contains('disableSpouseInput')){
+        element.parentElement.nextElementSibling.classList.remove('empShowError1518');
+        element.value='';
+        element.nextElementSibling.classList.remove('empLabelFocused1518');
+        // element.disabled=true;
+    }
 }
 
-disableSpouseNameField = () =>{
-    document.getElementById('spouseName').disabled = true
-    document.getElementById('spouseName').value=''
-}
 submitForm = () =>{
-    alert('Thank you!')
+    let noErrors =true;
+    console.log('entered in form');
+    
+    const textInputsLength = textInputs.length;
+    for(let cur = 0; cur<textInputsLength; cur++){
+        const element = textInputs[cur];
+        if(element.value==='' || element.value.trim().length==0){
+            showError(element);
+            noErrors=false;
+        } 
+        else if(!/^[a-zA-Z]+$/.test(element.value)){
+            element.parentElement.nextElementSibling.innerHTML = 'Please use only alphabets';
+            noErrors=false;
+        }
+    }
+    if(!tandcCheckbox.checked){
+        tandcCheckbox.parentElement.parentElement.nextElementSibling.innerHTML="Please check this";
+        return;
+    } 
+    
+    tandcCheckbox.parentElement.parentElement.nextElementSibling.innerHTML="";
+    if(noErrors){
+        toggleModal();
+    }
+    
+}
+
+resetForm = () =>{
+    textInputs.forEach(element =>{
+        element.value=""
+    })
+    textlabels.forEach(element =>{
+        element.classList.remove('empLabelFocused1518')
+    })
+    textArea.value=''
+    tandcCheckbox.checked=false;
+}
+
+toggleTooltip = () =>{
+    document.getElementsByClassName('empTooltip1518')[0].classList.toggle('empShowTooltip1518')
 }
 
 
-document.getElementById('married').addEventListener('click', enableSpouseNameField)
-document.getElementById('unmarried').addEventListener('click', disableSpouseNameField)
 
+
+document.getElementById('married').addEventListener('click', toggleSpouseNameField)
+document.getElementById('unmarried').addEventListener('click', toggleSpouseNameField)
+document.getElementById('empSubmitBtn1518').addEventListener('click', submitForm)
+document.getElementById('empResetBtn1518').addEventListener('click', resetForm)
+document.getElementsByClassName('empSpouseName1518')[0].addEventListener('mouseover',toggleTooltip)
+document.getElementsByClassName('empSpouseName1518')[0].addEventListener('mouseleave',toggleTooltip)
+document.getElementsByClassName('icon1518')[0].addEventListener('click', (event) =>{changeTheme(event)})
+document.querySelector('.empThankyouModalBody1518 span').addEventListener('click', toggleModal)
